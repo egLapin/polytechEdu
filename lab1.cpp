8fc8060cea348d5aa0735926e3646af35de2d32c
Lapin Egor <113190355+egLapin@users.noreply.github.com>
2022-09-16 18:27:52 +0300

    Create lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <locale>
+
+using namespace std;
+
+void main()
+{
+   setlocale( LC_ALL,"Russian" );
+    int aInt = 150;
+    float bFloat = 15.933;
+    short int cShInt = 250;
+
+    cout << "aInt" << aInt << endl;
+    cout << "bFloat" << bFloat << endl;
+    cout << "cShInt" << cShInt << endl;
+
+    short unsigned int Bdate = 21;
+    string Bmonth = "август";
+    short unsigned Byear = 2003;
+
+    cout << "Моя дата рождения: " << Bdate << "."
+         << Bmonth << "." << Byear << " года" << endl;
+
+    const float constFloat = 2.3;
+    const string constString = "WINDOWS";
+
+    cout << constFloat << " " << constString;
+}
